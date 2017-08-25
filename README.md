a06f4101a19f6b4ea148fffe0719dc216ce7f1d2
Jose Alanya <jose.alanya@gmail.com>
2017-08-25 11:26:49 -0500

    FEAT: Testing and Debugging - Demo: Mocha and Should.js

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 # Introduction to Node.js
 ## Demo: The Process object :smile:
 `$ kill -TERM <#process id>`
+
+## Testing and Debugging
+
+### Demo: Mocha and Should.js
+
+`./node_modules/.bin/mocha --help`  
+`./node_modules/.bin/mocha mocha.js`
+`./node_modules/.bin/mocha -R spec mocha.js`
+`./node_modules/.bin/mocha -R landing spec mocha.js`
---
+++ mocha.js
@@ -0,0 +1,48 @@
+const should = require('should');
+const fun = require('./mathfun');
+
+describe('MathFun', function() {
+
+  describe('when used synchronously', function() {
+
+    it('should double even numbers correctly', function() {
+
+      fun.evenDoublerSync(2).should.equal(4);
+
+    });
+
+    it('should throw on odd number', function(done) {
+
+      (function() { fun.evenDoublerSync(3) }).should.throw(/Odd/);
+      done();
+
+    });
+
+  });
+
+  describe('when used asynchronously', function() {
+
+      it('should double even numbers correctly', function(done) {
+
+        fun.evenDoubler(2, function(err, results) {
+
+          should.not.exist(err);
+          results.should.equal(4);
+          done();
+        });
+
+      });
+
+      it('should return error on odd numbers', function(done) {
+
+        fun.evenDoubler(3, function(err, results) {
+          should.exist(err);
+          should.not.exist(results);
+          done();
+        });
+
+      });
+
+  });
+
+});
